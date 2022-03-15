b6c841ee900f5b97df5c4ce0c8e826227ed24339
BFSmx4 <97541925+BFSmx4@users.noreply.github.com>
2022-03-15 15:29:57 -0600

    Update App.js

--- C88/project_template_Stellar-Stage-1-main/App.js
+++ C88/project_template_Stellar-Stage-1-main/App.js
@@ -13,13 +13,13 @@ const Stack = createStackNavigator();
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home" screenOptions={{
+      <Stack.Navigator initialRouteName="Inicio" screenOptions={{
         headerShown: false
       }}>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="StarMap" component={StartMapScreen} />
-        <Stack.Screen name="DailyPic" component={DailyPicScreen} />
-        <Stack.Screen name="SpaceCraft" component={SpaceCraftScreen} />
+        <Stack.Screen name="Inicio" component={HomeScreen} />
+        <Stack.Screen name="Mapa estelar" component={StartMapScreen} />
+        <Stack.Screen name="Imágenes diarias" component={DailyPicScreen} />
+        <Stack.Screen name="Naves espaciales" component={SpaceCraftScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
